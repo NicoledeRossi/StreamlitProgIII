d108550bdc084ac3883706cd79078273883ec844
Nicole <nickderossi@gmail.com>
2025-06-24 20:51:39 -0300

    inicio trabalho

---
+++ principal.py
@@ -0,0 +1,10 @@
+import streamlit as st
+#from utils.carrega_dados import carregar_dados
+
+st.set_page_config(
+    page_title="Frequencia Pacientes em Consultas",
+    page_icon="✨",
+    layout="wide"
+)
+
+st.title("Frequencia Pacientes em Consultas")
